75f42af578acb1701ae2a2add011f3c4ab0b0b04
MD ABDUL HADY <hady.onnorokom@gmail.com>
2022-06-08 09:03:00 +0600

    Designing

--- app/src/main/java/com/regin/reginald/vehicleanddrivers/Aariyan/Activity/LandingActivity.java
+++ app/src/main/java/com/regin/reginald/vehicleanddrivers/Aariyan/Activity/LandingActivity.java
@@ -3,8 +3,13 @@ package com.regin.reginald.vehicleanddrivers.Aariyan.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 import com.regin.reginald.vehicleanddrivers.Aariyan.Constant.Constant;
 import com.regin.reginald.vehicleanddrivers.Aariyan.Fragment.CreditRequestFragment;
@@ -15,6 +20,8 @@ import com.regin.reginald.vehicleanddrivers.R;
 public class LandingActivity extends AppCompatActivity {
 
     private ChipNavigationBar navigationBar;
+    private TextView toolbarTitle;
+    public static ImageView resetDatabaseIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +33,32 @@ public class LandingActivity extends AppCompatActivity {
     }
 
     private void initUI() {
+
+        toolbarTitle = findViewById(R.id.toolbarTitle);
+        resetDatabaseIcon = findViewById(R.id.resetDatabaseIcon);
+
         navigationBar = findViewById(R.id.menu);
         navigationBar.setItemSelected(R.id.home_menu, true);
 
         //setting the default Fragment:
         //set default fragment:
-        replaceFragment(new HomeFragment(),0);
+        replaceFragment(new HomeFragment(), 0);
 
         triggerNavigationBar();
     }
 
     private void replaceFragment(Fragment fragment, int position) {
-        setTitle(Constant.title[position]);
+        setTitles(position);
         getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+    }
+
+    private void setTitles(int position) {
+        if (position == 2) {
+            resetDatabaseIcon.setVisibility(View.VISIBLE);
+        } else {
+            resetDatabaseIcon.setVisibility(View.GONE);
+        }
+        toolbarTitle.setText(Constant.title[position]);
     }
 
     private void triggerNavigationBar() {
@@ -47,13 +67,13 @@ public class LandingActivity extends AppCompatActivity {
             public void onItemSelected(int i) {
                 switch (i) {
                     case R.id.home_menu:
-                        replaceFragment(new HomeFragment(),0);
+                        replaceFragment(new HomeFragment(), 0);
                         break;
                     case R.id.credit_request_menu:
-                        replaceFragment(new CreditRequestFragment(),1);
+                        replaceFragment(new CreditRequestFragment(), 1);
                         break;
                     case R.id.set_up_menu:
-                        replaceFragment(new SetupFragment(),2);
+                        replaceFragment(new SetupFragment(), 2);
                         break;
                 }
             }
--- app/src/main/java/com/regin/reginald/vehicleanddrivers/Aariyan/Fragment/SetupFragment.java
+++ app/src/main/java/com/regin/reginald/vehicleanddrivers/Aariyan/Fragment/SetupFragment.java
@@ -2,15 +2,22 @@ package com.regin.reginald.vehicleanddrivers.Aariyan.Fragment;
 
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.regin.reginald.vehicleanddrivers.Aariyan.Activity.LandingActivity;
 import com.regin.reginald.vehicleanddrivers.R;
 
 public class SetupFragment extends Fragment {
+
+    private View root;
+    private ConstraintLayout snackBarLayout;
 
     public SetupFragment() {
         // Required empty public constructor
@@ -27,6 +34,31 @@ public class SetupFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setup, container, false);
+        root = inflater.inflate(R.layout.fragment_setup, container, false);
+
+        initUI();
+        return root;
+    }
+
+    private void initUI() {
+        snackBarLayout = root.findViewById(R.id.snackBarLayout);
+        LandingActivity.resetDatabaseIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(snackBarLayout, getResources().getString(R.string.resetDatabaseTitle), Snackbar.LENGTH_SHORT)
+                        .setAction(getResources().getString(R.string.yes), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                resetDatabase();
+                            }
+                        }).show();
+            }
+        });
+
+    }
+
+    //Reset the database
+    private void resetDatabase() {
+        Toast.makeText(requireContext(), "Database Reset", Toast.LENGTH_SHORT).show();
     }
 }
