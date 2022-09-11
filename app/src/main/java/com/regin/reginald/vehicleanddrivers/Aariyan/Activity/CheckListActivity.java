7f748c25c62ca8fa0f3edb9717dbb36c52a9578d
MD ABDUL HADY <hady.onnorokom@gmail.com>
2022-09-11 12:33:16 +0600

    Check list design done

--- app/src/main/java/com/regin/reginald/vehicleanddrivers/Aariyan/Activity/CheckListActivity.java
+++ app/src/main/java/com/regin/reginald/vehicleanddrivers/Aariyan/Activity/CheckListActivity.java
@@ -3,6 +3,7 @@ package com.regin.reginald.vehicleanddrivers.Aariyan.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.regin.reginald.vehicleanddrivers.R;
 
@@ -12,5 +13,16 @@ public class CheckListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_list);
+
+        initUI();
+    }
+
+    private void initUI() {
+        findViewById(R.id.backBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
