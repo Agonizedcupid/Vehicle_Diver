7fe5ff988ca5056c57d63105854dc30860c8eb79
Agonizedcupid <agonizedcupid@gmail.com>
2022-09-14 16:27:36 +0600

    Writing UI variable for CheckListActivity

--- app/src/main/java/com/regin/reginald/vehicleanddrivers/Aariyan/Activity/CheckListActivity.java
+++ app/src/main/java/com/regin/reginald/vehicleanddrivers/Aariyan/Activity/CheckListActivity.java
@@ -4,10 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.regin.reginald.vehicleanddrivers.R;
 
+import org.w3c.dom.Text;
+
 public class CheckListActivity extends AppCompatActivity {
+
+    private TextView dateSelect, submitBtn;
+    private Spinner vehicleSpinner, driverSpinner;
+
+    private RadioButton radiatorYesBtn,radiatorNoBtn;
+    private RadioButton engineOilYesBtn,engineOilNoBtn;
+    private RadioButton breakFluidYesBtn,breakFluidNoBtn;
+    private RadioButton tyreConditionYesBtn,tyreConditionNoBtn;
+    private RadioButton tyreSpareYesBtn,tyreSpareNoBtn;
+    private RadioButton jackYesBtn,jackNoBtn;
+    private RadioButton spannerYesBtn,spannerNoBtn;
+    private RadioButton fireYesBtn,fireNoBtn;
+    private RadioButton capsWaterBtn,capsWaterNoBtn;
+    private RadioButton capsFuelYesBtn,capsFuelNoBtn;
+    private RadioButton dipstickYesBtn,dipstickNoBtn;
+    private RadioButton windScreenYesBtn,windScreenNoBtn;
+    private RadioButton sideMirrorsYesBtn,sideMirrorsNoBtn;
+    private RadioButton lightMainYesBtn,lightMainNoBtn;
+    private RadioButton lightIndicatorsYesBtn,lightIndicatorsNoBtn;
+    private RadioButton brakesYesBtn,brakesNoBtn;
+    private RadioButton cabBodyYesBtn,cabBodyNoBtn;
+    private RadioButton fridgeWaterYesBtn,fridgeWaterNoBtn;
+    private RadioButton fridgeOilYesBtn,fridgeOilNoBtn;
+    private RadioButton fridgeBeltsYesBtn,fridgeBeltsNoBtn;
+    private RadioButton fridgeDipstickYesBtn,fridgeDipstickNoBtn;
+    private RadioButton fridgeBodyYesBtn,fridgeBodyNoBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
